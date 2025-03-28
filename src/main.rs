c2a131c56a5a44d71f7510aa8f9c38436c9e5c23
Rishab Karki <karkirishab78@gmail.com>
2025-03-28 11:57:43 +0545

    Functional Approach

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use tokio::net::UdpSocket;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() {
+    server_listener().await.unwrap();
+}
+
+async fn server_listener() -> std::io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:29953").await?;
 
+    println!("Listening to logs on port 29953");
+
     let mut buf = [0u8; 1024];
-
-    println!("Listening to logs on port 27005");
-
     loop {
         let (len, addr) = socket.recv_from(&mut buf).await?;
         println!("{:?} bytes received from {:?}", len, addr);
 
         println!("Received: {}", String::from_utf8_lossy(&buf[..len]));
-
-        let len = socket.send_to(&buf[..len], addr).await?;
-        println!("{:?} bytes sent", len);
     }
 }
